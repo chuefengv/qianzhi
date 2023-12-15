69332902f16733dbb985e249081495409d726a60
chuefengv <feng.vang98@gmail.com>
2023-12-15 12:29:09 -0500

    added titles to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+        <div>
+            <h1>Qianzhi Shen</h1>
+            <h2>Steadicam Operator</h2>
+        </div>
+        <nav>
+            <div>Past Work</div>
+            <div>Equipment</div>
+        </nav>
+    </>
   )
 }
