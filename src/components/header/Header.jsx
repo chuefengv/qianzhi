7523a7bd701ca920c5fac372bc069d066c833ea5
chuefengv <feng.vang98@gmail.com>
2023-12-19 20:28:13 -0500

    set up the header component class name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,15 +3,15 @@ import "../../app/globals.css"
 
 export default function Header() {
   return (
-    <>
-        <div className='red'>
+    <div className='header-component'>
+        <div className='title'>
             <h1>Qianzhi Shen</h1>
-            <h2>Steadicam Operator</h2>
+            <h4>Steadicam Operator</h4>
         </div>
-        <nav>
+        <nav className='nav-bar'>
             <div>Past Work</div>
             <div>Equipment</div>
         </nav>
-    </>
+    </div>
   )
 }
