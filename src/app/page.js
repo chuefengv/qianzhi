10d64d5b5a6572afe56c5089eba7622d9bed82d8
chuefengv <feng.vang98@gmail.com>
2023-12-19 20:14:30 -0500

    test save

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Header from "@/components/header/Header"
 import Body from "@/components/body/Body"
 import Footer from "@/components/footer/Footer"
+import "./globals.css"
+
 export default function Home() {
   return (
     <main>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../../app/globals.css"
 
 export default function Header() {
   return (
     <>
-        <div>
+        <div className='red'>
             <h1>Qianzhi Shen</h1>
             <h2>Steadicam Operator</h2>
         </div>
