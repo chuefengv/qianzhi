ba546bf2dd12d59af85d572d9397b0bc818b1c20
chuefengv <feng.vang98@gmail.com>
2023-12-19 23:30:54 -0500

    rough layout of footer set up

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main>
       <Header />
       <Body />
-      {/* <Footer /> */}
+      <Footer />
     </main>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-component'>
+      <div className='reach-out'>
+        <p>REACH OUT</p>
+        <p>Qianzhi Shen</p>
+        <p>NEW YORK</p>
+      </div>
+      <div className='contact-links'>
+        <p>Instagram</p>
+        <p>Email</p>
+      </div>
+    </div>
   )
 }
