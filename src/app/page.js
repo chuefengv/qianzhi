26df0b376f4a91cf58d9df14d7d320720c71e70e
chuefengv <feng.vang98@gmail.com>
2023-12-23 17:50:16 -0500

    changed body components to demoreel componenets

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Header from "@/components/header/Header"
-import Body from "@/components/body/Body"
+import Demoreel from "@/components/demoreel/Demoreel"
 import Footer from "@/components/footer/Footer"
 import "./globals.css"
 
 export default function Home() {
   return (
     <main>
-      <Body />
+      <Demoreel />
     </main>
   )
 }
--- src/components/demoreel/Demoreel.jsx
+++ src/components/demoreel/Demoreel.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import "../../app/globals.css"
 import ReactPlayer from 'react-player/youtube'
 
-export default function Body() {
+export default function Demoreel() {
 
   const URL = "https://www.youtube.com/watch?v=ABJJQLeVlqA&ab_channel=QianzhiShen"
   //checks the window to see if there is content, if there content then load the video player
@@ -16,7 +16,7 @@ export default function Body() {
   }, []);
 
   return (
-    <div className='body-component'>
+    <div className='demoreel'>
       <iframe className="video-player" src="https://www.youtube.com/embed/ABJJQLeVlqA?si=GH0cKTreCREuMo6F" title="YouTube video player" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   )
