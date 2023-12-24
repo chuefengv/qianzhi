dc007dd0712d561bea7f4ec3adec6598b3792b54
chuefengv <feng.vang98@gmail.com>
2023-12-24 17:54:10 -0500

    added s3 bucket links to all the project images, added projects to project page

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -4,7 +4,13 @@ import { projectlist } from "../projectlist"
 export default function projects() {
   return (
     <div className="projects">
-
+        {projectlist.map((video,index)=>{
+            return(
+                <div className="image-wrapper">
+                    <img src={video.image} alt={video.title} />
+                </div>
+            )
+        })}
     </div>
   )
 }
