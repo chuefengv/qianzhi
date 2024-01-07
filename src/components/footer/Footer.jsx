d8df13973901cf0c445da4605da7f81ee72f401a
chuefengv <feng.vang98@gmail.com>
2024-01-07 02:36:33 -0500

    cleared footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,23 +1,11 @@
 'use client'
 
 import React from 'react'
-import { Icon } from '@iconify/react';
 
 export default function Footer() {
   return (
     <div className='footer-component'>
-      <div className='reach-out'>
-        <p>REACH OUT</p>
-        <p>NEW YORK CITY</p>
-      </div>
-      <div className='contact-links'>
-        <a href='/'>
-          <Icon icon="iconoir:instagram" className='contact-icons'/>
-        </a>
-        <a href='/'>
-          <Icon icon="carbon:email" className='contact-icons'/>
-        </a>
-      </div>
+
     </div>
   )
 }
