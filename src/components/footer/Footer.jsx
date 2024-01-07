b9fe375fc2d51f9abba31316cb315b8087edfa6a
chuefengv <feng.vang98@gmail.com>
2024-01-07 03:17:59 -0500

    added dotted borders

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,15 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footer-component'>
-
+      <section>
+        <ul>
+          <h4>Contact Me</h4>
+          <li>TsyenShen@fakeemail.com</li>
+          <li>Resume</li>
+          <li>Instagram</li>
+          <li>IMDB</li>
+        </ul>
+      </section>
     </div>
   )
 }
