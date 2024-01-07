e55a7bd6e3f07183260c547148530d242c410bdf
chuefengv <feng.vang98@gmail.com>
2024-01-07 01:22:53 -0500

    carousel did not work, scapped, going for simple static layout

--- src/app/equipment/page.jsx
+++ src/app/equipment/page.jsx
@@ -1,19 +1,32 @@
 'use client'
 import React from 'react'
 import "../../app/globals.css"
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { btsimages } from '../btsimages';
+import { btsimages } from '../btsimages'
+import Image from 'next/image'
 
 export default function equipment() {
 
   return (
     <div className='equipment'>
-      <div className='carousel-container'>
-        <Carousel className='carousel' emulateTouch={true} swipeable={true} infiniteLoop={true} showStatus={false} showArrows={false} showThumbs={false}>
-          {btsimages.map((image,index) => <img key={index} src={image.url} alt='this sialt text'/>)}
-        </Carousel>
-      </div>      
+
+      <div className='image-container'>
+        <img src={btsimages[7].url} alt='trets' />
+      </div>
+      
+      <section className='text-container'>
+        <h3>My Current Gear:</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi tenetur accusantium nemo mollitia, unde consectetur, aliquam delectus cum voluptatum aut repudiandae. Illo tenetur reprehenderit magnam similique quis facilis eum sit.</p>
+        <ul>
+          <li>MK-V Nexus Sled</li>
+          <li>Master Series Vest</li>
+          <li>NB Stabalizer Arm</li>
+          <li>SmallHD 703 Monitor</li>
+          <li>Modern 1 Riser Steadicam Stand with Wheels</li>
+          <li>Anton Bauer Batteries</li>
+          <li>Rock and Roller Cart</li>
+        </ul>
+      </section>
+
     </div>
   )
 }
