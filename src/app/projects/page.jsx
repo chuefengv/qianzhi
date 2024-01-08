6c5bef8bf4543b0124a05e4b942761cc01af5442
chuefengv <feng.vang98@gmail.com>
2024-01-07 22:27:19 -0500

    swapped out video modal

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,18 +1,31 @@
-import "../../app/globals.css"
+'use client'
+import { useState } from "react"
 import { projectlist } from "../projectlist"
+import "../../app/globals.css"
+import "../../../node_modules/react-modal-video/scss/modal-video.scss"
+import ModalVideo from 'react-modal-video'
 
 export default function projects() {
+
+  const [isOpen, setOpen] = useState(false)
+  const [videoId,setVideoId] = useState("")
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
   return (
     <div className="projects">
+        <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="L61p2uyiMSo" onClose={onCloseModal} />
+
+      
         {projectlist.map((video,index)=>{
             return(
-                <div className="image-wrapper">
+                <div className="image-wrapper" onClick={onOpenModal} >
                     <img key={index} src={video.image} alt={video.title} />
                     <div className="image-title">
                       <h4>{video.title}</h4>
                     </div>
                 </div>
-              
             )
         })}
     </div>
