9a2e1ca440822fcb7f4126f242d13e5552cbbde9
chuefengv <feng.vang98@gmail.com>
2024-01-08 00:19:35 -0500

    fixed margins on header to make mobile work

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body className="layout">
         <Header />
