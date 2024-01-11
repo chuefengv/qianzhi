bccc199f6ead145a91e6c00b10c4f406e38b0e20
chuefengv <feng.vang98@gmail.com>
2024-01-10 22:56:33 -0500

    added new pictures to about section, added shadows to pictures and thumbnails

--- src/app/btsimages.jsx
+++ src/app/btsimages.jsx
@@ -7,4 +7,9 @@ export const btsimages = [
     {index:6, url:'https://qianzhishenequipment.s3.amazonaws.com/bts6.JPEG'},
     {index:7, url:'https://qianzhishenequipment.s3.amazonaws.com/bts7.jpg'},
     {index:8, url:'https://qianzhishenequipment.s3.amazonaws.com/bts8.JPG'},
+    {index:9, url:'https://qianzhishenequipment.s3.amazonaws.com/bts9.jpeg'},
+    {index:10, url:'https://qianzhishenequipment.s3.amazonaws.com/bts10.jpeg'},
+    {index:11, url:'https://qianzhishenequipment.s3.amazonaws.com/bts11.jpeg'},
+    {index:12, url:'https://qianzhishenequipment.s3.amazonaws.com/bts12.jpeg'},
+
 ]
